bb02c69e829e62ac3e5a77a6ddee2158d386b31c
YatesTech <soviet@presburn.tk>
2013-11-21 21:07:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ from Tkinter import *
 import sys, os
 import tkFont
 top = Tk()
-
+top.geometry('640x480+200+200')
 #Fonts Settings
 headfont = tkFont.Font(family="Helvetica", size=24)
+headtwofont = tkFont.Font(family="Helvetica", size=18)
+headlinetitle = tkFont.Font(family="Helvetica", size=14)
 
 #Title
-head = Label(top, text = "Presburn \n \n \n", font=headfont)
+head = Label(top, text = "Presburn \n ", font=headfont)
 head.pack()
 top.title("Presburn")
+
+#News
+hlines = Label(top, text = "Headlines \n ", font=headtwofont)
+hlines.pack()
+hlinesone = Label(top, text = "Headline One Title \n", font=headlinetitle)
+hlinesone.pack()
+#Citizenship
+
 
 top.mainloop()
