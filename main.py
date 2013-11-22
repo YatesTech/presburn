c9fe5e4b474d32f5d0394db3eb7f217aea8ab4a7
YatesTech <soviet@presburn.tk>
2013-11-21 20:46:47 -0600

    Update main.py
    Title & Header Font

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from Tkinter import *
 import sys, os
-
+import tkFont
 top = Tk()
 
-head = Label(top, text = "Presburn \n \n \n")
+#Fonts Settings
+headfont = tkFont.Font(family="Helvetica", size=24)
+
+#Title
+head = Label(top, text = "Presburn \n \n \n", font=headfont)
 head.pack()
+top.title("Presburn")
+
 top.mainloop()
