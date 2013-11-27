1ca779cf6efbc9010ed79f063518460bd60c8ac9
YatesTech <soviet@presburn.tk>
2013-11-27 13:03:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,19 @@ headfont = tkFont.Font(family="Helvetica", size=24)
 headtwofont = tkFont.Font(family="Helvetica", size=18)
 headlinetitle = tkFont.Font(family="Helvetica", size=14)
 
-#Title
-head = Label(top, text = "Presburn \n ", font=headfont)
-head.pack()
-top.title("Presburn")
+def main():
+  #Title
+  head = Label(top, text = "Presburn \n ", font=headfont)
+  head.pack()
+  top.title("Presburn")
 
-#News
-#ONLY ONE HEADLINE!
-hlines = Label(top, text = "Headlines \n ", font=headtwofont)
-hlines.pack()
-hlinesone = Label(top, text = "Headline One Title \n", font=headlinetitle)
-hlinesone.pack()
+  #News
+  #ONLY ONE HEADLINE!
+  hlines = Label(top, text = "Headlines \n ", font=headtwofont)
+  hlines.pack()
+  hlinesone = Label(top, text = "Headline One Title \n", font=headlinetitle)
+  hlinesone.pack()
 #Citizenship
-
-
+def citizenship
+  
 top.mainloop()
