6fb8156a8c3451c337d0787df9ad92cb4612fc92
YatesTech <soviet@presburn.tk>
2013-11-30 19:15:54 -0600

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,16 +1,6 @@
-
-
-echo "Checking for git"
-sudo apt-get install git
-
-echo "Checking for python"
 sudo apt-get install python
-
-echo "Updating Presburn Application"
-git clone https://github.com/YatesTech/presburn.git
-echo "Moving to the required directory."
-cd presburn-master
-echo "Making Program Launchable"
-chmod 755 main.py
-echo "Launching..."
-./main.py
+sudo apt-get install git
+git pull https://github.com/YatesTech/presburn.git
+clear
+cd presburn
+python main.py
