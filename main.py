696ec20d1a86529f2e5d6285c21da65dbbdb59bf
BraydenYates <soviet@presburn.tk>
2013-12-01 12:01:18 -0600

    Updated news

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ print ("Welcome to the Presburn Application Menu!")
 #News Headline
 print (" \n \n \n News ")
 #Actual News
-print (" \n \n \n Sat 30 Nov, 2013 @ 7:13PM ")
-print ("Presburn may become capitalist. \n Today a bill was proposed that could make Presburn a \n capitalist state. The poll is currently: \n 2/0/0 (Support/Oppose/Abstain) \n The poll ends tomorow.")
+print (" \n \n \n ")
+print ("The bill to make Presburn capitalist has passed.   ")
 
 #info
 
