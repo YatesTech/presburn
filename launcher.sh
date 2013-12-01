79b098aae0822df507cad791692d0a4e4c05e3e4
YatesTech <soviet@presburn.tk>
2013-11-30 19:25:58 -0600

    Update launcher.sh
    Now removes folder "presburn" Deleting Caroline just now taught me something. Sometimes, the best way to solve a problem is the easiest.  *portal reference*

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,6 @@
-sudo apt-get install python
-sudo apt-get install git
-git pull https://github.com/YatesTech/presburn.git
+rm -rf presburn
+git clone https://github.com/YatesTech/presburn.git
 clear
 cd presburn
 python main.py
+rm -rf presburn
