32e8d4d4d4df1e9b356de13bb4a53bb2c31cfd98
BraydenYates <soviet@presburn.tk>
2013-12-01 12:06:43 -0600

    Killed some whitespace!

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import sys, os
 print ("Welcome to the Presburn Application Menu!")
 #News Headline
-print (" \n \n \n News ")
+print (" \n News ")
 #Actual News
-print (" \n \n \n ")
+print (" \n ")
 print ("The bill to make Presburn capitalist has passed.   ")
 
 #info
 
 #breaking the two spaces
-print ("\n \n \n \n ")
+print (" \n ")
 #start info
 print ("Want citizenship? \n To get citizenship, go to our website at http://www.presburn.tk/ \n")
 #IRC info
