fa2916c54c95c1651c5333973121fd81ee5312b9
BraydenYates <soviet@presburn.tk>
2013-11-30 23:42:00 -0600

    updated with email info

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ print ("\n \n \n \n ")
 #start info
 print ("Want citizenship? \n To get citizenship, go to our website at http://www.presburn.tk/ \n")
 #IRC info
-print ("To get in contact with us, go to our IRC channel on freenode: #presburn")
+print ("To get in contact with us, go to our IRC channel on freenode: #presburn. \n")
